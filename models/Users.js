aa4106dc6a02ad8174c6ebd5117ac9c053f3e16e
StockDC2 <StockDC2@gmail.com>
2016-12-11 17:29:02 -0800

    add user authentication

--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,25 @@
+// Creating the blueprint for our 'User' model.
+
 var mongoose = require('mongoose');
+// Crypto is a native node module for hashing passwords.
+var crypto = require('crypto');
 
 var UserSchema = new mongoose.Schema({
     username: {type: String, lowercase: true, unique: true},
     hash: String,
     salt: String
 });
+
+UserSchema.model.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+};
+
+UserSchema.model.validPassword = function(password) {
+    var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+    return this.hash === hash;
+}
 
 mongoose.model('User', UserSchema);
